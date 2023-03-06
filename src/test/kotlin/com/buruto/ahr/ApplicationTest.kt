87c39b08b4a703b58615efe98f9c915032716120
abdulhafizramadan-dev <abdulhafizramadan.dev@gmail.com>
2023-03-06 20:00:06 +0700

    | Test Root Endpoint

--- src/test/kotlin/com/buruto/ahr/ApplicationTest.kt
+++ src/test/kotlin/com/buruto/ahr/ApplicationTest.kt
@@ -2,20 +2,18 @@ package com.buruto.ahr
 
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import io.ktor.server.testing.*
-import kotlin.test.*
 import io.ktor.http.*
-import com.buruto.ahr.plugins.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            assertEquals("Welcome to Boruto API", bodyAsText())
+
         }
     }
 }
